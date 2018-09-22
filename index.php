c9d487a4cc66d601dbbdccc1a81b0aa601da3ccc
Chathurangi6 <37058133+Chathurangi6@users.noreply.github.com>
2018-09-22 12:35:31 +0530

    created signup form

--- index.php
+++ index.php
@@ -14,4 +14,19 @@
 				<input type="password" name="pass" placeholder="password"><br><br>
 				<button style="background-color:#5B70F7;color:white" type="submit"><b>Login</b></button>
 			</form>
+			<form action="signup.php" method="post">
+				<h2>Don't have an account? sign up here</h2>
+				<label>Username </label>
+				<input type="text" name="uname" placeholder="User name"><br><br>
+				<label>Email </label>
+				<input type="email" name="email" placeholder="E-mail"><br><br>
+				<label>Password </label>
+				<input type="password" name="pass" placeholder="password"><br><br>
+				<button style="background-color:#5B70F7;color:white" type="submit"><b>Signup</b></button>
+			</form>
+
+		</div>
+	</div>
+
 </body>
+</html>
