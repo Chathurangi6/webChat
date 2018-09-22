5a477b200dd52999c72974810aa57e7e75547990
Chathurangi6 <37058133+Chathurangi6@users.noreply.github.com>
2018-09-22 12:16:46 +0530

    created login form

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>WebChat</title>
+</head>
+<body>
+	<div id="main">
+		<div id="info">
+			<h2>login here</h2>
+			<form action="login.php" method="post">
+				<label><b>Username</b></label>
+				<input type="text" name="uname" placeholder="User name"><br><br>
+				<label><b>Password</b></label>
+				<input type="password" name="pass" placeholder="password"><br><br>
+				<button style="background-color:#5B70F7;color:white" type="submit"><b>Login</b></button>
+</body>
